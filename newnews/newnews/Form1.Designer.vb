ce305681cbf26fd6912b2fc8e720d103f307d68f
vinitkumariitg <46778276+vinitkumariitg@users.noreply.github.com>
2019-02-11 13:32:29 +0530

    Add files via upload
    code for add news in add_prof_info page

---
+++ newnews/newnews/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(167, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "news"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(170, 92)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(332, 132)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(106, 92)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(46, 132)
+        Me.RichTextBox2.TabIndex = 2
+        Me.RichTextBox2.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(106, 34)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(396, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(106, 379)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 35)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "updatenews"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 356)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "changenews"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(36, 314)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 17)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "NewsID"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(106, 351)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(396, 22)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(106, 314)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(396, 22)
+        Me.TextBox3.TabIndex = 9
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(25, 33)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "addnewsnew"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(103, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 17)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "newsID"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(648, 504)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ newnews/newnews/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Database2.accdb")
+    ' Dim connString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Database2.accdb"
+    Dim dr As OleDbDataReader
+    ' Dim ds As New DataSet
+    ' Dim da As OleDb.OleDbDataAdapter
+    ' Dim indec As Integer
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conn.Open()
+
+        Dim are As Integer = 2
+        Dim id As Int32 = 1
+        Dim query As String = "select * from newstable1"
+        Dim cmd As New OleDbCommand(query, conn)
+        'da.Fill(ds, "mynews")
+        dr = cmd.ExecuteReader()
+        While dr.Read
+            'If () Then
+            If dr("prof_id") = id Then
+                RichTextBox1.AppendText(dr("news2") & vbNewLine)
+                RichTextBox2.AppendText(dr("ID") & vbNewLine)
+            End If
+
+        End While
+        dr.Close()
+        conn.Close()
+    End Sub
+
+    Private Sub btnupdatenews_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        conn.Open()
+        Dim query As String = "select * from newstable1"
+        Dim cmd As New OleDbCommand(query, conn)
+
+        Dim text As String = TextBox2.Text
+        Dim id_num As Integer = CInt(TextBox3.Text)
+        cmd.CommandText = "UPDATE newstable1 SET news2 ='" & text & "'WHERE ID = " & id_num & ";"
+
+
+        Try
+            cmd.ExecuteNonQuery()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            cmd.Dispose()
+        End Try
+
+        conn.Close()
+        Me.Close()
+    End Sub
+
+ 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        conn.Open()
+        'Dim query As String = "select * from newstable1"
+        'Dim cmd As New OleDbCommand(query, conn)
+
+        Dim textnews As String = TextBox1.Text
+        Dim profId As Integer = 1
+        Dim query As String
+        query = "INSERT INTO newstable1([prof_id],[news2]) Values (?,?)"
+        Dim cmd As OleDbCommand = New OleDbCommand(query, conn)
+        cmd.Parameters.Add(New OleDbParameter("prof_id", CType(profId, Integer)))
+        cmd.Parameters.Add(New OleDbParameter("news2", CType(textnews, String)))
+        Try
+            cmd.ExecuteNonQuery() 'Executing Update Command
+            cmd.Dispose()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message) 'Error Message
+        End Try
+
+        conn.Close()
+        Me.Close()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+End Class
